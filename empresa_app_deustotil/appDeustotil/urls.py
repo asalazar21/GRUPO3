c9f94a5be20a0948c11b358204f2ee80f50e2276
Aitor <aitor.salazar@opendeusto.es>
2024-04-19 18:05:50 +0200

    urls corregido

--- empresa_app_deustotil/appDeustotil/urls.py
+++ empresa_app_deustotil/appDeustotil/urls.py
@@ -5,17 +5,17 @@ from django.urls import path
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
 
-    path('tickets', views.ProyectoListView.as_view(), name='index_proyectos'),
-    path('tickets/<int:pk>/', views.ProyectoDetailView.as_view(), name='detail_proyectos'),
-    path('tickets/<int:pk>/borrar/', views.ProyectoDeleteView.as_view(), name='delete_proyectos'),
+    path('proyectos', views.ProyectoListView.as_view(), name='index_proyectos'),
+    path('proyectos/<int:pk>/', views.ProyectoDetailView.as_view(), name='detail_proyectos'),
+    path('proyectos/<int:pk>/borrar/', views.ProyectoDeleteView.as_view(), name='delete_proyectos'),
     
-    path('tickets', views.TareaListView.as_view(), name='index_proyectos'),
-    path('tickets/<int:pk>/', views.TareaDetailView.as_view(), name='detail_proyectos'),
-    path('tickets/<int:pk>/borrar/', views.TareaDeleteView.as_view(), name='delete_proyectos'),
+    path('tareas', views.TareaListView.as_view(), name='index_proyectos'),
+    path('tareas/<int:pk>/', views.TareaDetailView.as_view(), name='detail_proyectos'),
+    path('tareas/<int:pk>/borrar/', views.TareaDeleteView.as_view(), name='delete_proyectos'),
     
-    path('tickets', views.EmpleadoListView.as_view(), name='index_proyectos'),
-    path('tickets/<int:pk>/', views.EmpleadoDetailView.as_view(), name='detail_proyectos'),
-    path('tickets/<int:pk>/borrar/', views.EmpleadoDeleteView.as_view(), name='delete_proyectos'),
+    path('empleados', views.EmpleadoListView.as_view(), name='index_proyectos'),
+    path('empleados/<int:pk>/', views.EmpleadoDetailView.as_view(), name='detail_proyectos'),
+    path('empleados/<int:pk>/borrar/', views.EmpleadoDeleteView.as_view(), name='delete_proyectos'),
 
    
 ]
